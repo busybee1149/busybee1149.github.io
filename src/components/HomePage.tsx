82a9792ff2e3acfdec4183c132ed20e0cf5ad7ad
Dinesh <dinecents@gmail.com>
2021-01-14 13:10:59 +0530

    setup travelmap with leaflet & OSM

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -3,7 +3,7 @@ import Menu from './NavMenu';
 
 const Title = () =>
     ( <div className="titleCentered">
-                <h1 className="mainTitle"> @Dinesh Vijayakumar </h1>
+                <h1 className="mainTitle"> @Dinesh Vijayakumar  </h1>
                 <pre className="subTitle"> Technologist | Travel Enthusiast| Philanthropist </pre>
             </div>
 );
--- src/components/TravelMap.tsx
+++ src/components/TravelMap.tsx
@@ -1,11 +1,41 @@
 import React from "react";
-//import './index.css';
+import { MapContainer, TileLayer, Marker, Tooltip } from 'react-leaflet';
 
 
 function Travelmap() {
     return (
-        <div id="mapid">
-             <p> Map Here </p>
+        <div>
+        <MapContainer center={[10.9591, 76.8988]} zoom={2} scrollWheelZoom={false} style={{ height: "60vh" }}>
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[10.9591, 76.8988]}>
+                <Tooltip>
+                    Coimbatore <br /> Home.
+                </Tooltip>
+            </Marker>
+
+            <Marker position={[51.51, -0.06]}>
+                 <Tooltip> London </Tooltip>
+            </Marker>
+
+            <Marker position={[-13.52264, -71.96734]}>
+                 <Tooltip> Cusco </Tooltip>
+            </Marker>
+            <Marker position={[-13.1631, -72.5450]}>
+                 <Tooltip> Machu Picchu </Tooltip>
+            </Marker>
+        </MapContainer>
+
+        <div>
+            <h1> Places I have travelled </h1>
+                <ol>
+                    <li> London </li>
+                    <li> Cusco </li>
+                    <li> Machhu Picchu </li>
+                </ol>
+        </div>
         </div>
     )
 }
