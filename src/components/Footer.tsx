543045251b68a2480edbaa27e257e11b03d86be4
Dinesh <dinecents@gmail.com>
2021-02-05 09:05:24 -0800

    add social media icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,9 +5,12 @@ function Footer() {
     <div className="footer">
       <footer className="py-3 fixed-bottom">
         <div className="footer-container">
-          <p className="m-0 text-center text-black">
+          <pre className="m-0 text-center text-black ">
+              <a href="https://www.linkedin.com/in/dineshvijaykumar"><img alt="main" src={process.env.PUBLIC_URL + '/images/linkedin.png'} /></a>
+              <a href="https://github.com/busybee1149"><img alt="main" src={process.env.PUBLIC_URL + '/images/github.png'} /></a>
+             <br />
             Copyright &copy; Dinesh Vijayakumar 2021
-          </p>
+          </pre>
         </div>
       </footer>
     </div>
