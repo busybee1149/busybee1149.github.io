667e9973a1cb2a4a2d9ac4fab776d076e01f3dd2
Dinesh <dinecents@gmail.com>
2021-02-20 15:54:16 -0800

    minor updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,8 +5,8 @@ function Footer() {
      <div className="footer">
         <footer className="py-3 fixed-bottom">
             <pre className="m-0 text-center text-black ">
-                <a href="https://www.linkedin.com/in/dineshvijaykumar"><img alt="main" src={process.env.PUBLIC_URL + '/images/linkedin.png'}/> </a>
-                <a href="https://github.com/busybee1149"><img alt="main" src={process.env.PUBLIC_URL + '/images/github.png'}/> </a>
+                <a href="https://www.linkedin.com/in/dineshvijaykumar"><img alt="main" src={process.env.PUBLIC_URL + '/images/linkedin.png'}/></a>
+                <a href="https://github.com/busybee1149"><img alt="main" src={process.env.PUBLIC_URL + '/images/github.png'}/></a>
                 Copyright &copy; Dinesh Vijayakumar 2021
             </pre>
         </footer>
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -3,16 +3,32 @@ import React from "react";
 function HomePage() {
     return (
         <div className="main">
-            <section>
-                <h1 className="mainTitle"> @Dinesh Vijayakumar  </h1>
-                <pre className="subTitle"> Technologist | Travel Enthusiast| Philanthropist </pre>
-             </section>
-            <figure className="figurefx pushup">
-                <img alt="main" src={process.env.PUBLIC_URL + '/images/logo192.png'} />
-                <figcaption>
-                    Photo of Dinesh Vijayakumar
-                </figcaption>
-            </figure>
+            <div id="homeTitle" >
+                <section>
+                    { <h1 className="mainTitle"> @Dinesh Vijayakumar  </h1> }
+                    <pre className="subTitle"> Technologist | Travel Enthusiast| Philanthropist </pre>
+                </section>
+             </div>
+            <div id="homeContent"> 
+               <div id="homeFigure" >
+                    <figure className="figurefx pushup">
+                        <img alt="main" src={process.env.PUBLIC_URL + '/images/logo192.png'} />
+                        <figcaption>
+                            Photo of Dinesh Vijayakumar
+                        </figcaption>
+                    </figure>
+                </div>
+               <div id="homeIntro">
+                    <p>
+                        Hello there!!, I'm Dinesh Vijayakumar, 
+                        a technologist working at Amazon, 
+                        enthusiastic to travel around the world, 
+                        genuinely compassionate at heart,  
+                        interested in philanthropy,
+                        as well as philosophy.
+                    </p>
+                </div>
+            </div> 
         </div>
     );
 }
